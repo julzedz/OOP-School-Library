e61f8410854b1fe274896d8d509983fda3d0e8ec
Edozie <edoziechukwuma@gmail.com>
2023-03-17 13:50:34 -0600

    Fix errors

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -2,7 +2,7 @@ class Teacher < Person
   attr_accessor :specialization
 
   def initialize(age, specialization, parent_permission: true, name: 'Unknown')
-    super(age, name: name, parent_permission: parent_permission)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
