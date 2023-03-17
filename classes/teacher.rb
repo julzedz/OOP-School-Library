04a64292eeea37fa55ae853ecf409f8ab09a27f3
Edozie <edoziechukwuma@gmail.com>
2023-03-17 13:25:09 -0600

    Correct linter errors

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,7 +1,7 @@
-class Teacher < Person 
-  attr_accessor :specialization 
+class Teacher < Person
+  attr_accessor :specialization
 
-  def initialize(age, parent_permission: true, name: 'Unknown', specialization: nil )
+  def initialize(age, parent_permission: true, name: 'Unknown', specialization: nil)
     super(age, name: name, parent_permission: parent_permission)
     @specialization = specialization
   end
