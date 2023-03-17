f3c0fd4d5df03a2418e6bdae9cc8eb7c902f9a91
Edozie <edoziechukwuma@gmail.com>
2023-03-17 07:10:01 -0600

    Add class teacher

---
+++ classes/teacher.rb
@@ -0,0 +1,12 @@
+class Teacher < Person 
+  attr_accessor :specialization 
+
+  def initialize(age, parent_permission: true, name: 'Unknown', specialization: nil )
+    super(age, name: name, parent_permission: parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
