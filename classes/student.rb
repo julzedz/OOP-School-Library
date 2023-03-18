d8bb21ba19819afdf4b4a902cb45cd435c9b25bc
Jules Edozie <edoziechukwuma@gmail.com>
2023-03-18 22:59:29 +0100

    Modify student constructor

--- classes/student.rb
+++ classes/student.rb
@@ -3,10 +3,9 @@ require_relative './person'
 class Student < Person
   attr_accessor :classroom, :type
 
-  def initialize(age, name, parent_permission, classroom: nil)
+  def initialize(classroom, age, name, parent_permission: true, type: 'Student')
     super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
-    # classroom.students.push(self) unless classroom.students.include?(self)
     @type = type
   end
 
